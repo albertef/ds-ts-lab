74493da4a314d9feb3d6cba5e8f84b7abeb0c322
albertef <albertef@gmail.com>
2026-01-31 14:43:06 +0000

    Define types using Utility types.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,17 +1,17 @@
 import { Friend, Colleague, ColleagueHistory } from "./myTypes";
 
-const friend1: Friend  = {
+const friend1: Friend = {
   name: "Paul Fleming",
   phone: "087-12345",
   age: 25,
-  dob: new Date("1998-11-20") ,
+  dob: new Date("1998-11-20"),
 };
 
-const friend2: Friend  = {
+const friend2: Friend = {
   name: "Jane Costello",
   phone: "086--12345",
   age: 31,
-  interests: ['Music', 'Sport']
+  interests: ["Music", "Sport"],
 };
 
 export const friends: Friend[] = [friend1, friend2];
@@ -19,7 +19,7 @@ export const friends: Friend[] = [friend1, friend2];
 
 //   -------------------
 const colleague1: Colleague = {
-  name: "Ralph Graham",
+  name: "Paul Fleming",
   department: "Engineering",
   contact: {
     email: "rgraham@company.com",
